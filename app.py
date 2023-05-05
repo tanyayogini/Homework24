178b17570eb9ba5ce6aae0a4499c22eac2b8abc3
tanyayogini <tvkushnareva@yandex.ru>
2023-05-05 14:30:28 +0500

    docker build

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import os
+import psycopg2
 from typing import Union
 
 from flask import Flask, request, jsonify, Response
 from marshmallow import ValidationError
+from sqlalchemy import text
 
 from builder import build_query
+from config import Config
+from db import db
 from models import RequestSchema
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
@@ -15,7 +21,6 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 @app.route("/perform_query", methods=["POST"])
 def perform_query() -> Union[Response, tuple[Response, int]]:
-
     data = request.json
     try:
         RequestSchema().load(data)
@@ -29,3 +34,23 @@ def perform_query() -> Union[Response, tuple[Response, int]]:
     first_result = build_query(cmd=data['cmd1'], value=data['value1'], file_name=file_path, data=None)
     second_result = build_query(cmd=data['cmd2'], value=data['value2'], file_name=file_path, data=first_result)
     return jsonify(second_result)
+
+
+@app.route('/app-test/')
+def app_check():
+    return "Application is working"
+
+
+@app.route('/db-test/')
+def db_check():
+    result = db.session.execute(
+        text(
+            'SELECT 1'
+        )
+    ).scalar()
+
+    return jsonify(
+        {
+            'result': result,
+        }
+    )
